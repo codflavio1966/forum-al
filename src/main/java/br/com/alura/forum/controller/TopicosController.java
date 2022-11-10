260f5a2d87f3128b998d7f3fc9203a782b5354fe
javaflavio <flaviomattos.40@gmail.com>
2022-11-09 22:46:40 -0300

    teste commit win5

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -18,7 +18,7 @@ public class TopicosController {
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
 		
-		Topico topico = new Topico("Duvida putinha", "Duvida com Spring", new Curso("Spring", "Programação"));
+		Topico topico = new Topico("Duvida putinha gospel", "Duvida com Spring", new Curso("Spring", "Programação"));
 		
 		return TopicoDto.converter(Arrays.asList(topico, topico, topico)); 
 		
